ca5169f5cf1fba438b4ffd053329e5bb907d97de
kishansudani <48625369+kishansudani@users.noreply.github.com>
2023-01-31 22:34:21 +0530

    Create save_all_images.sh

---
+++ scripts/Docker/save_all_images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script will save all your local Docker images as tar.gz files.
+
+echo "Checking your images"
+
+for img in `sudo docker images | awk '$1 != "REPOSITORY" { print $1 }' | sort | uniq` ; do
+    zip_file=${img//\//_}.tar.gz
+    echo "Saving $img to ${zip_file}"
+    sudo docker save $img | gzip >  ${zip_file}
+done
+
+echo "Save all images into local gz files done."
+echo "You can restore them as images using docker load *.tar.gz."
